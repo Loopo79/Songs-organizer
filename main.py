ecda94de007321a0da8c64188ff1ad90e3bed8e3
Loopo79 <155375560+Loopo79@users.noreply.github.com>
2026-04-14 17:21:58 +0530

    Edit main.py - added better error handling

--- main.py
+++ main.py
@@ -2,34 +2,45 @@ import mutagen
 from pathlib import Path
 import os
 import shutil
+import re
 
 songs_dir = Path('songs/')
 albums_dir = Path('albums/')
 
+
+def is_valid_folder_name(folder_name: str) -> bool:
+    return not re.search('[/\0]', folder_name)
+
 tracks_by_album: dict[str, list[Path]] = {}
 for song_path in songs_dir.iterdir():
-    # mutagen.File(song_path, easy=True)['album'] returns ['album_name']
-    album_name:str = mutagen.File(song_path, easy=True)['album'][0]
-    
+    try:
+        # mutagen.File(song_path, easy=True)['album'] returns ['album_name']
+        album_name:str = mutagen.File(song_path, easy=True)['album'][0]
+
+    except:
+        print(f"Error: the file {song_path} is either not a audio file or does not have the album tag.")
+        print(f"Skipping {song_path}")
+        continue
+        
     tracks_by_album.setdefault(album_name, list()).append(song_path)
     
 
 songs_count = albums_count = 0
-for album_name in tracks_by_album:
-    valid_album_name = album_name
+for album_key in tracks_by_album:
+    album_name = album_key
     while True:
-        try:
-            os.makedirs(albums_dir/valid_album_name, exist_ok=True)
-            print("Created directory:", albums_dir/valid_album_name)
+        if is_valid_folder_name(album_name):
+            os.makedirs(albums_dir/album_name, exist_ok=True)
+            print("Created directory:", albums_dir/album_name)
             albums_count += 1
             break
 
-        except:
-            print("Invalid directory name:", valid_album_name)
-            valid_album_name = input("Enter a valid album name: ")
+        else:
+            print("Error: Invalid directory name:", album_name)
+            album_name = input("Enter a valid album name: ")
 
-    for song in tracks_by_album[album_name]:
-        shutil.move(song, albums_dir/valid_album_name/song.name)
+    for song in tracks_by_album[album_key]:
+        shutil.move(song, albums_dir/album_name/song.name)
         songs_count += 1
 
 print(f"\nCreated {albums_count} album folders with {songs_count} songs.")
